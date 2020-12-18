375c0a5831321512ef8222b18f6f0a50462b14ff
petko <petko.mikov.uk@gmail.com>
2020-12-18 12:18:29 +0000

    env variables2

--- final_web_project/settings.py
+++ final_web_project/settings.py
@@ -138,7 +138,6 @@ STATIC_ROOT =os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")

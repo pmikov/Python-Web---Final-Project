f32535cef8c0e5e66465d284d56be205a44dbdf7
petko <petko.mikov.uk@gmail.com>
2020-12-18 12:15:07 +0000

    env variables

--- final_web_project/settings.py
+++ final_web_project/settings.py
@@ -86,11 +86,11 @@ WSGI_APPLICATION = 'final_web_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'final_web_project',
-        'USER': 'postgres',
-        'PASSWORD': 'petko1984',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'NAME': os.environ.get("DB_NAME"),
+        'USER': os.environ.get("DB_USER"),
+        'PASSWORD':os.environ.get("DB_PASSWORD"),
+        'HOST': os.environ.get("DB_HOST"),
+        'PORT': os.environ.get("DB_PORT"),
     }
 }
 
